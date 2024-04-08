c6165ba94d13424ba3047968a6afd9c7d86b5148
Yunus Santur <patron@192.168.1.10>
2024-04-08 23:08:18 +0300

    function based views

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TweetListView, TweetCreateView
+#from .views import TweetListView, TweetCreateView
+from tweets import views as tweets_views
 
 urlpatterns = [
-    path('tweets/new/', TweetCreateView.as_view(), name='tweet_new'),
-    path('', TweetListView.as_view(), name='home'),
+    #path('tweets/new/', TweetCreateView.as_view(), name='tweet_new'),
+    #path('', TweetListView.as_view(), name='home'),
+    path('tweets/new/', tweets_views.tweet_new, name='tweet_new'),
+    path('', tweets_views.tweet_list, name='home'),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -3,6 +3,25 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse
 from .models import Tweet
 
+from django.shortcuts import render, redirect
+from .models import Tweet
+from .forms import TweetForm
+
+def tweet_list(request):
+    return render(request, 'home.html', {'object_list': Tweet.objects.all()})
+
+def tweet_new(request):
+    if request.method == 'POST':
+        form = TweetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TweetForm
+
+    return render(request, 'tweet_new.html', {'form': form })
+
+    
 class TweetListView(ListView):
     model = Tweet
     template_name = 'home.html'
